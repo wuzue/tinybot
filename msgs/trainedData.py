e08b405737401f876aae226b63d1b3ee68f082ac
Renan Araújo <renan.araujoonline@gmail.com>
2023-03-06 06:42:18 -0300

    added multiple datasets to tinybot

--- msgs/trainedData.py
+++ msgs/trainedData.py
@@ -9,14 +9,13 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 bot = ChatBot('tinybot')
 trainer = ChatterBotCorpusTrainer(bot)
-# trainer.train('chatterbot.corpus.english.greetings')
-trainer.train('chatterbot.corpus.english.')
+trainer.train('chatterbot.corpus.english')
 
-@app.route('/process-message', methods=['GET', 'POST'])
+@app.route('/process-message', methods=['GET','POST'])
 def process_message():
-    message = request.json.get('message')
-    response = bot.get_response(message)
-    return jsonify({'message': str(response)}) #returns json, to return only string -> return response
+  message = request.json.get('message')
+  response = bot.get_response(message)
+  return jsonify({'message': str(response)})
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
